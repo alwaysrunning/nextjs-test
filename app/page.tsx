91c5d79c2a8e5ef570e006d1b22a06799e2412be
yanghaitao <178224406@qq.com>
2025-12-17 11:23:01 +0800

    通过api获取types数据

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,24 @@ import PokemonGrid from './components/PokemonGrid';
 import PaginationControls from './components/PaginationControls';
 
 const PAGE_SIZE = 24;
-// 没找到相关接口，先写死
-const TYPES = ['normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy', 'stellar', 'unknown'];
 
 export const metadata: Metadata = {
   title: 'IT Consultis',
 };
+
+async function getTypes() {
+  const res = await fetch('https://pokeapi.co/api/v2/type', {
+    cache: 'no-store',
+  });
+  
+  if (!res.ok) {
+    throw new Error('Failed to fetch types');
+  }
+  
+  const data = await res.json();
+  
+  return data.results.map((item: { name: string; url: string }) => item.name);
+}
 
 async function getData(page: number) {
   const offset = (page - 1) * PAGE_SIZE;
@@ -36,6 +48,7 @@ export default async function Page(props: {
   const searchParams = await props.searchParams;
   const currentPage = Number(searchParams?.page) || 1;
   const currentType = searchParams?.type || '';
+  const types = await getTypes();
   const data = await getData(currentPage);
   const totalPages = Math.ceil(data.count / PAGE_SIZE);
 
@@ -46,7 +59,7 @@ export default async function Page(props: {
           Welcome to Pokemon world
       </div>
       <div>Total: {data.count}</div>
-      <TypeFilter types={TYPES} currentType={currentType} />
+      <TypeFilter types={types} currentType={currentType} />
       <PokemonGrid items={data.results} />
       <PaginationControls currentPage={currentPage} totalPages={totalPages} />
     </div>
